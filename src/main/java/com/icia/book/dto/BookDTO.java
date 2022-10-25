e9ad4e0bb3b33bdf8a9d951da8ca656f1f4b9200
hs1709 <su1152124@naver.com>
2022-10-25 10:53:36 +0900

    Initial commit

---
+++ src/main/java/com/icia/book/dto/BookDTO.java
@@ -0,0 +1,17 @@
+package com.icia.book.dto;
+
+import lombok.*;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString
+
+public class BookDTO {
+    private Long bookId;
+    private String bookName;
+    private String bookAuthor;
+    private int bookPrice;
+    private String bookPublisher;
+}
